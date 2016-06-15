f68dcaff746af2599b1de5800047ae7c5cf3fddb
Greg <gregt87@gmail.com>
2016-06-15 02:20:46 -0400

    added comment of what code should be

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ app.get('/api/json_style', function (req, res) {
 	console.log("inside api style")
   thematic.renderCSS({
   	cwd: __dirname,
-    file: '../node_modules/sanitize.css/sanitize.css',
-    varsData: '$color1: blue; $color2: red;',//vars to be replaced default
-    themeData: '$color1: red; $color2: green;',
+    file: '../node_modules/sanitize.css/sanitize.css', //this should be site themes as scss... not css... mistake
+    varsData: '$color1: blue; $color2: red;', //default set of vars
+    themeData: '$color1: red; $color2: green;', //replacing sert of vars 
     includePaths: ['../style/']
   }, function(err, cssString) {
 		console.log("err = %j", err);
